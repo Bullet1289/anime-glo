b431a165ddb084d281101dc50b4ad607cd43b62c
Bullet1289 <smorkachev95@mail.ru>
2024-02-27 21:19:43 +0300

    first

---
+++ js/bg-elements.js
@@ -0,0 +1,13 @@
+const elements = document.querySelectorAll ('.set-bg')
+
+for (let i = 0; i < elements.length; i++) {
+    
+    
+    const src = elements[i].dataset.setbg
+
+    // elements[i].style.backgroundImage = 'url(' + src + ')'
+    elements[i].style.backgroundImage = `url(${src})`
+
+    // console.log(src);
+
+}
---
+++ js/model.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector(".search-model")
+
+const modalBtn = document.querySelector(".icon_search")
+
+const modalClose = modal.querySelector('.search-close-switch')
+console.log(modalClose);
+
+modalBtn.addEventListener('click', () => {
+    modal.style.display = 'block'
+
+    console.log('click'); 
+})
+
+modalClose.addEventListener('click', () => {
+    modal.style.display = 'none'
+
+    console.log('click'); 
+})
